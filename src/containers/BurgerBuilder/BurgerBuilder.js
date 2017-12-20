cde4234add17214e48ab5c85d34a414538bfde28
elliot richardson <ear78@aol.com>
2017-12-19 23:51:41 -0600

    setup firebase server and hooked up axios with post request confirmed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -28,7 +29,7 @@ class BurgerBuilder extends React.Component {
 			purchasing: false
 		};
 	}
-	
+
 	updatePurchaseState = (ingredients) => {
 		const sum = Object.keys(ingredients)
 		.map(igKey => {
@@ -37,12 +38,12 @@ class BurgerBuilder extends React.Component {
 		.reduce((sum, el) => {
 			return sum + el;
 		}, 0);
-		
+
 		this.setState({
 			purchaseable: sum > 0
 		})
 	}
-	
+
 	addIngredientHandler = (type) => {
 		const oldCount = this.state.ingredients[type];
 		const updatedCount = oldCount + 1;
@@ -50,18 +51,18 @@ class BurgerBuilder extends React.Component {
 			...this.state.ingredients
 		};
 		updatedIngredients[type] = updatedCount;
-		
+
 		const priceAddition = INGREDIENT_PRICES[type];
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice + priceAddition;
-		
+
 		this.setState({
 			totalPrice: newPrice,
 			ingredients: updatedIngredients
 		});
 		this.updatePurchaseState(updatedIngredients);
 	}
-	
+
 	removeIngredientHandler = (type) => {
 		const oldCount = this.state.ingredients[type];
 		if(oldCount <= 0){
@@ -72,30 +73,47 @@ class BurgerBuilder extends React.Component {
 			...this.state.ingredients
 		};
 		updatedIngredients[type] = updatedCount;
-		
+
 		const priceDeduction = INGREDIENT_PRICES[type];
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice - priceDeduction;
-		
+
 		this.setState({
 			totalPrice: newPrice,
 			ingredients: updatedIngredients
 		});
 		this.updatePurchaseState(updatedIngredients);
 	}
-	
+
 	purchaseHandler = () => {
 		this.setState({purchasing: true})
 	}
-	
+
 	purchaseCancelHandler = () => {
 		this.setState({purchasing: false})
 	}
-	
+
 	purchaseContinueHandler = () => {
-		alert('Please Continue!');
+		// alert('Please Continue!');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Elliot',
+				address: {
+					street: 'Test Street 1',
+					zipCode: '54433',
+					country: 'Germany'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'Fastest'
+		}
+		axios.post('/orders.json', order)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
 	}
-	
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -112,7 +130,7 @@ class BurgerBuilder extends React.Component {
 					price={this.state.totalPrice}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls 
+				<BuildControls
 				ingredientAdded={this.addIngredientHandler}
 				ingredientRemove={this.removeIngredientHandler}
 				disabled={disabledInfo}
