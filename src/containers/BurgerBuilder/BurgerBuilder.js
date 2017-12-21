94549cd6e0001532c99d1b198c8c152a41995ac1
elliot richardson <ear78@aol.com>
2017-12-21 00:00:51 -0600

    added spinner functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -26,7 +27,8 @@ class BurgerBuilder extends React.Component {
 			},
 			totalPrice: 4,
 			purchaseable: false,
-			purchasing: false
+			purchasing: false,
+			loading: false
 		};
 	}
 
@@ -95,6 +97,9 @@ class BurgerBuilder extends React.Component {
 
 	purchaseContinueHandler = () => {
 		// alert('Please Continue!');
+		this.setState({
+			loading: true
+		})
 		const order = {
 			ingredients: this.state.ingredients,
 			price: this.state.totalPrice,
@@ -109,9 +114,13 @@ class BurgerBuilder extends React.Component {
 			},
 			deliveryMethod: 'Fastest'
 		}
-		axios.post('/orders.json', order)
-			.then(response => console.log(response))
-			.catch(error => console.log(error));
+		// axios.post('/orders.json', order)
+		// 	.then(response => {
+		// 		this.setState({ loading: false, purchasing: false })
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({ loading: false, purchasing: false })
+		// 	});
 	}
 
 	render() {
@@ -121,13 +130,19 @@ class BurgerBuilder extends React.Component {
 		for(let key in disabledInfo){
 			disabledInfo[key] = disabledInfo[key] <= 0;
 		}
+		// conditionally setting order summary
+		let orderSummary = <OrderSummary ingredients={this.state.ingredients} cancel={this.purchaseCancelHandler}
+		continue={this.purchaseContinueHandler}
+		price={this.state.totalPrice}/>;
+
+		if(this.state.loading){
+			orderSummary = <Spinner />
+		}
 		return (
 			<Aux>
 				{/*modal passes props down to backdrop and setState with purchasing to toggle*/}
 				<Modal show={this.state.purchasing} modalClose={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} cancel={this.purchaseCancelHandler}
-					continue={this.purchaseContinueHandler}
-					price={this.state.totalPrice}/>
+					{orderSummary}
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
